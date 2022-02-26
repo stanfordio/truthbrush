ec3f9cac428b1df2a377e6a6c84cdf6563368d79
R. Miles McCain <github@sendmiles.email>
2022-02-25 20:23:53 -0800

    Respect rate limits

--- client/api.py
+++ client/api.py
@@ -1,7 +1,10 @@
 from email.generator import Generator
+from time import sleep
 from typing import Any, Iterator, List
 from loguru import logger
+from dateutil import parser as date_parse
 import requests
+from datetime import datetime, timezone
 
 BASE_URL = "https://truthsocial.com/api/v1"
 USER_AGENT = "TruthSocial/45 CFNetwork/1329 Darwin/21.3.0"
@@ -9,23 +12,50 @@ USER_AGENT = "TruthSocial/45 CFNetwork/1329 Darwin/21.3.0"
 class Api:
     def __init__(self, auth_id):
         self.auth_id = auth_id
+        self.ratelimit_max = 300
+        self.ratelimit_remaining = None
+        self.ratelimit_reset = None
+
+    def _check_ratelimit(self, resp):
+        if resp.headers.get("x-ratelimit-limit") is not None:
+            self.ratelimit_max = int(resp.headers.get("x-ratelimit-limit"))
+        if resp.headers.get("x-ratelimit-remaining") is not None:
+            self.ratelimit_remaining = int(resp.headers.get("x-ratelimit-remaining"))
+        if resp.headers.get("x-ratelimit-reset") is not None:
+            self.ratelimit_reset = date_parse.parse(resp.headers.get("x-ratelimit-reset"))
+
+        if self.ratelimit_remaining is not None and self.ratelimit_remaining <= 50: # We do 50 to be safe; their tracking is a bit stochastic... it can jump down quickly
+            now = datetime.utcnow().replace(tzinfo=timezone.utc)
+            time_to_sleep = (self.ratelimit_reset.replace(tzinfo=timezone.utc) - now).total_seconds()
+            logger.warning(f"Approaching rate limit; sleeping for {time_to_sleep} seconds...")
+            sleep(time_to_sleep)
+
 
     def _get(self, url: str, params: dict=None) -> Any:
-        return requests.get(BASE_URL + url, params=params, headers={
+        resp = requests.get(BASE_URL + url, params=params, headers={
             "authorization": "Bearer " + self.auth_id,
             "user-agent": USER_AGENT,
-        }).json()
+        })
+
+        # Will also sleep
+        self._check_ratelimit(resp)
+
+        return resp.json()
 
     def _get_paginated(self, url: str, params: dict=None) -> Any:
-        next_link = url
+        next_link = BASE_URL + url
         while next_link is not None:
-            resp = requests.get(BASE_URL + url, params=params, headers={
+            resp = requests.get(next_link, params=params, headers={
                 "authorization": "Bearer " + self.auth_id,
                 "user-agent": USER_AGENT,
             })
 
             next_link = resp.links.get('next', {}).get('url')
+            logger.info(f"Next: {next_link}, resp: {resp}, headers: {resp.headers}")
             yield resp.json()
+
+            # Will also sleep
+            self._check_ratelimit(resp)
 
     def lookup(self, user_handle: str=None) -> dict:
         """Lookup a user's information."""
