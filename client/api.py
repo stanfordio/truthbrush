f7098bb4516fc5ff8f6fb49f5c3d9a995e931dcd
David Thiel <dthiel@stanford.edu>
2022-03-24 13:59:43 +0000

    Add support for the trends endpoint and blacken

--- client/api.py
+++ client/api.py
@@ -10,7 +10,8 @@ from datetime import datetime, timezone
 from urllib3 import Retry
 
 BASE_URL = "https://truthsocial.com/api/v1"
-USER_AGENT = "TruthSocial/45 CFNetwork/1329 Darwin/21.3.0"
+USER_AGENT = "TruthSocial/54 CFNetwork/1331 Darwin/21.4.0"
+
 
 class Api:
     def __init__(self, auth_id):
@@ -21,7 +22,11 @@ class Api:
 
     def _make_session(self):
         s = requests.Session()
-        retries = Retry(total=10, backoff_factor=0.5, status_forcelist=[413, 429, 503, 403, 500, 503])
+        retries = Retry(
+            total=10,
+            backoff_factor=0.5,
+            status_forcelist=[413, 429, 503, 403, 500, 503],
+        )
         s.mount("http://", HTTPAdapter(max_retries=retries))
         s.mount("https://", HTTPAdapter(max_retries=retries))
         return s
@@ -32,48 +37,70 @@ class Api:
         if resp.headers.get("x-ratelimit-remaining") is not None:
             self.ratelimit_remaining = int(resp.headers.get("x-ratelimit-remaining"))
         if resp.headers.get("x-ratelimit-reset") is not None:
-            self.ratelimit_reset = date_parse.parse(resp.headers.get("x-ratelimit-reset"))
+            self.ratelimit_reset = date_parse.parse(
+                resp.headers.get("x-ratelimit-reset")
+            )
 
-        if self.ratelimit_remaining is not None and self.ratelimit_remaining <= 50: # We do 50 to be safe; their tracking is a bit stochastic... it can jump down quickly
+        if (
+            self.ratelimit_remaining is not None and self.ratelimit_remaining <= 50
+        ):  # We do 50 to be safe; their tracking is a bit stochastic... it can jump down quickly
             now = datetime.utcnow().replace(tzinfo=timezone.utc)
-            time_to_sleep = (self.ratelimit_reset.replace(tzinfo=timezone.utc) - now).total_seconds()
-            logger.warning(f"Approaching rate limit; sleeping for {time_to_sleep} seconds...")
+            time_to_sleep = (
+                self.ratelimit_reset.replace(tzinfo=timezone.utc) - now
+            ).total_seconds()
+            logger.warning(
+                f"Approaching rate limit; sleeping for {time_to_sleep} seconds..."
+            )
             sleep(time_to_sleep)
 
-
-    def _get(self, url: str, params: dict=None) -> Any:
-        resp = self._make_session().get(BASE_URL + url, params=params, headers={
-            "authorization": "Bearer " + self.auth_id,
-            "user-agent": USER_AGENT,
-        })
+    def _get(self, url: str, params: dict = None) -> Any:
+        resp = self._make_session().get(
+            BASE_URL + url,
+            params=params,
+            headers={
+                "authorization": "Bearer " + self.auth_id,
+                "user-agent": USER_AGENT,
+            },
+        )
 
         # Will also sleep
         self._check_ratelimit(resp)
 
         return resp.json()
 
-    def _get_paginated(self, url: str, params: dict=None) -> Any:
+    def _get_paginated(self, url: str, params: dict = None) -> Any:
         next_link = BASE_URL + url
         while next_link is not None:
-            resp = self._make_session().get(next_link, params=params, headers={
-                "authorization": "Bearer " + self.auth_id,
-                "user-agent": USER_AGENT,
-            })
+            resp = self._make_session().get(
+                next_link,
+                params=params,
+                headers={
+                    "authorization": "Bearer " + self.auth_id,
+                    "user-agent": USER_AGENT,
+                },
+            )
 
-            next_link = resp.links.get('next', {}).get('url')
+            next_link = resp.links.get("next", {}).get("url")
             logger.info(f"Next: {next_link}, resp: {resp}, headers: {resp.headers}")
             yield resp.json()
 
             # Will also sleep
             self._check_ratelimit(resp)
 
-    def lookup(self, user_handle: str=None) -> dict:
+    def lookup(self, user_handle: str = None) -> dict:
         """Lookup a user's information."""
-        
+
         assert user_handle is not None
         return self._get("/accounts/lookup", params=dict(acct=user_handle))
 
-    def user_followers(self, user_handle: str=None, user_id: str=None, maximum: int=1000) -> Iterator[dict]:
+    def trending(self):
+        """Return trending truths."""
+
+        return self._get("/truth/trending/truths")
+
+    def user_followers(
+        self, user_handle: str = None, user_id: str = None, maximum: int = 1000
+    ) -> Iterator[dict]:
         assert user_handle is not None or user_id is not None
         user_id = user_id if user_id is not None else self.lookup(user_handle)["id"]
 
--- client/cli.py
+++ client/cli.py
@@ -11,9 +11,18 @@ from .api import Api
 load_dotenv()  # take environment variables from .env.
 api = Api(os.getenv("TRUTHSOCIAL_TOKEN"))
 
+
 @click.group()
 def cli():
     """This is an API client for Truth Social."""
+
+
+@cli.command()
+def trends():
+    """Pull a user's metadata."""
+
+    print(json.dumps(api.trending()))
+
 
 @cli.command()
 @click.argument("handle")
@@ -22,10 +31,11 @@ def user(handle: str):
 
     print(json.dumps(api.lookup(handle)))
 
+
 @cli.command()
 @click.argument("handle")
 @click.option("--maximum", help="the maximum number of followers to pull", type=int)
-def followers(handle: str, maximum: int=None):
+def followers(handle: str, maximum: int = None):
     """Pull a user's followers."""
 
     for follower in api.user_followers(handle, maximum=maximum):
