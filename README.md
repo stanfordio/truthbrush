51f64ec86f32757c950a4c9d1755c2720ebdc3eb
David Thiel <dthiel@stanford.edu>
2022-05-29 16:12:26 +0100

    Add CLI for grabbing a user's followed users.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ API client for Truth Social
 ```text
 Commands:
   followers    Pull a user's followers.
+  followers    Pull users that a given user follows.
   statuses     Pull a user's statuses.
   suggestions  Pull the list of suggested users.
   tags         Pull trendy tags.
--- truthbrush/cli.py
+++ truthbrush/cli.py
@@ -63,6 +63,23 @@ def followers(handle: str, maximum: int = None, resume: str = None):
 
 
 @cli.command()
+@click.argument("handle")
+@click.option(
+    "--maximum", help="the maximum number of followed users to pull", type=int
+)
+@click.option(
+    "--resume",
+    help="the `max_id` cursor to resume from, if necessary (pull this from logs to resume a failed/stalled export)",
+    type=str,
+)
+def followers(handle: str, maximum: int = None, resume: str = None):
+    """Pull users a given user follows."""
+
+    for followed in api.user_following(handle, maximum=maximum, resume=resume):
+        print(json.dumps(followed))
+
+
+@cli.command()
 @click.argument("username")
 @click.option(
     "--replies/--no-replies",
