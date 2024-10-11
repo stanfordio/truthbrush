34767f42722c558576c074c67bca0902375bfe6d
MJ Rossetti <s2t2@users.noreply.github.com>
2024-10-11 18:54:34 -0400

    Helper function

---
+++ truthbrush/utils.py
@@ -0,0 +1,12 @@
+from datetime import timezone
+from dateutil import parser as date_parse
+
+
+"""Utility Functions"""
+
+
+def as_datetime(date_str):
+    """Datetime formatter function. Ensures timezone is UTC.
+
+    """
+    return date_parse.parse(date_str).replace(tzinfo=timezone.utc)
